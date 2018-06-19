d252b84237c04e55a15c6d140003f2dcf004c55d
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-06-19 13:20:03 -0400

    half done

--- board.js
+++ board.js
@@ -4,45 +4,70 @@ function Board(height, width, gridElement) {
     this.height = height
     this.width = width
     this.gridElement = gridElement
-    this.grid = []
-    this.sampleArray = new Array(this.height).fill().map(() => new Array(this.width).fill(0))
+    this.grid = new Array(this.height).fill().map(() => new Array(this.width).fill())
+    this.sampleArray = new Array(this.height).fill()
 }
 Board.prototype = {
 
     createBoard: function (height, width, element) {
         this.createElement()
-        this.grid.push([])
-        this.sampleArray.forEach((row, rowIndex) => {
-            rowIndex.forEach((_,cellIndex) => {
-                sampleArray[rowIndex][cellIndex] = new place()
-            } 
-        )
-    })
-        console.log(this.sampleArray)
-        for (let rowIndex = 0; rowIndex < this.height; rowIndex++) {
+        this.grid.forEach((_, rowIndex) => {
 
-            fetch(`http://jservice.io/api/category?id=${rowIndex+1}`)
-                .then(response => response.json())
-                .then(category => {
-                    const rowElement = document.createElement("div")
-                    rowElement.classList.add("row")
-                    this.element.appendChild(rowElement)
-                    this.sampleArray.push(category)
-                    for (let colIndex = 0; colIndex < this.width; colIndex++) {
-                        let cell = new Cell(rowIndex, colIndex, rowElement, category)
-                        this.grid[rowIndex].push(cell)
-                    }
-                })
-        }
+            const rowElement = document.createElement("div")
+            rowElement.classList.add("row")
+            this.element.appendChild(rowElement)
+
+            for (let colIndex = 0; colIndex < this.width; colIndex++) {
+                this.grid[rowIndex][colIndex] = new Cell(rowIndex, colIndex, rowElement)
+            }
+
+            this.fetchData(rowIndex, 1)
+        })
     },
+
     createElement: function (gridElement) {
 
         this.element = document.createElement("div")
         this.element.id = "grid"
         this.gridElement.appendChild(this.element)
+
     },
+
+    fetchData: function (rowIndex, offset) {
+        fetch(`http://jservice.io/api/category?id=${rowIndex + offset}`)
+            .then(response => response.json())
+            .then(category => {
+                console.log(category)
+                if (category.clues.length <= this.width) {
+                    this.fetchData(rowIndex, offset + 1)
+                    return
+                }
+
+                this.assignCategory(category, rowIndex)
+
+                this.sampleArray[rowIndex].forEach((categoryClue, cellIndex) => {
+                    const text = (cellIndex === 0) ?
+                        document.createTextNode(category.title) :
+                        document.createTextNode(categoryClue.question)
+                        
+                        this.grid[rowIndex][cellIndex].cellElement.appendChild(text)
+                 
+
+                })
+            })
+    },
+
+    assignCategory: function (category, rowIndex) {
+        let startingPoint = Math.floor(Math.random() * category.clues.length - this.width) + 1
+        if (startingPoint < 1) startingPoint = 1
+
+        this.sampleArray[rowIndex] = category.clues.slice(startingPoint, startingPoint + this.width)
+        this.sampleArray[rowIndex].title = category.title
+    },
+
     constructor: Board,
 }
+
 const inputs = new Board(6, 6, document.getElementById("output"))
 
 inputs.createBoard()
