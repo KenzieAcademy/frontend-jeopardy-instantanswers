fc6b63007faf19e1426e47bcd13b2fc87bdb6b45
bschue <bschuess3@gmail.com>
2018-06-19 16:16:51 -0400

    game logic is coming along

--- cell.js
+++ cell.js
@@ -3,7 +3,6 @@ function Cell(colIndex, rowIndex, parentElement, grid) {
     this.colIndex = colIndex
     this.parentElement = parentElement
     this.grid = grid
-    // console.log(this.grid)
 
     this.add_element()
 }
@@ -17,16 +16,5 @@ Cell.prototype = {
         this.element.dataset.rowIndex = this.rowIndex
         this.element.dataset.colIndex = this.colIndex
         this.parentElement.appendChild(this.element)
-        this.element.addEventListener("click", this.handle_click)
-    },
-
-    handle_click: function (event) {
-        let cellclicked = event.target;
-        let clickedRow = Number(cellclicked.dataset.rowIndex)
-        let clickedCol = Number(cellclicked.dataset.colIndex)
-        console.log(this.grid)
-        // this.grid.findCellByIndex(clickedRow,clickedCol)
-        console.log(clickedRow,clickedCol);
-        // this.checkneighbor_cells();
-    },
+    }
 }
--- jeopardy.js
+++ jeopardy.js
@@ -1,17 +1,24 @@
-function JeopardyGrid(numberOfCols, numberOfRows, parentElement) {
+function JeopardyGrid(numberOfCols, numberOfRows, parentElement, enableCORSProxy = false) {
     Grid.call(this, numberOfRows, numberOfCols, parentElement)
     this.random_categories_array = []
     this.random_categories_number_arr = []
     this.values_array = []
+    this.enableCORSProxy = enableCORSProxy
+    this.CORSProxy = "https://cors-anywhere.herokuapp.com/"
 
     this.fetchFromJService("categories?count=100")
         .then(categories => {
             this.board_create()
+                // find a way
+                // to make sure that
+                // if the category selected has less than 5 clues("questions"),
+                // another category gets chosen
                 .chooseRandomCategories(categories)
                 .drawCategories(categories)
                 .fetchQuestions(categories)
         })
 }
+let onBoardCategories = [];
 
 JeopardyGrid.prototype = Object.create(Grid.prototype)
 JeopardyGrid.prototype.constructor = JeopardyGrid
@@ -30,7 +37,8 @@ JeopardyGrid.prototype.board_create = function () {
 
 JeopardyGrid.prototype.chooseRandomCategories = function (categories) {
     for (let j = 0; j < this.numberOfCols; j++) {
-        randomNum = Math.floor(Math.random() * categories.length);
+        const randomNum = Math.floor(Math.random() * categories.length);
+        // console.log(categories[randomNum])
         this.random_categories_array.push(categories[randomNum])
         this.random_categories_number_arr.push(categories[randomNum].id)
     }
@@ -41,27 +49,18 @@ JeopardyGrid.prototype.drawCategories = function (categories) {
         let categoryTitle = this.random_categories_array[m].title
         this.board[m][0].element.textContent = categoryTitle
         this.board[m][0].element.classList.add("category")
-        //console.log(categoryTitle)
-        //console.log(this.board[m][0])
     }
     return this
 }
 
 JeopardyGrid.prototype.fetchQuestions = function (categories) {
-    for (let m = 0; m < this.random_categories_array.length; m++) {
-        let categoriesIDs = this.random_categories_array[m].id
-        //console.log(this.values_array)
-
-        this.fetchFromJService(`clues?category=${categoriesIDs}`)
-            .then(values => {
-                this.values_array.push(values)
-                //console.log(values)
-                this.drawQuestions(values)
-                // return
-                // for( let rowNum = 1 ; rowNum < this.numOfRows ; rowNum++ ){
-                //   this.grid[rowNum][currentCol].element.textContent = rowNum * 200;
-                // }
-                //console.log("hey", category , currentCol );         
+    for (let catIndex = 0; catIndex < this.random_categories_array.length; catIndex++) {
+        let catID = this.random_categories_array[catIndex].id
+        this.fetchFromJService(`clues?category=${ catID }`)
+            .then(category => {
+                this.values_array.push(category)
+                onBoardCategories.push(category)
+                this.drawQuestions(category, catIndex)
             })
 
     }
@@ -69,7 +68,8 @@ JeopardyGrid.prototype.fetchQuestions = function (categories) {
 }
 
 JeopardyGrid.prototype.fetchFromJService = function (endpoint) {
-    return fetch(`http://cors-anywhere.herokuapp.com/http://jservice.io/api/${endpoint}`)
+    const uri = `${ this.enableCORSProxy ? this.CORSProxy : "" }http://jservice.io/api/${ endpoint }`
+    return fetch(uri)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -77,9 +77,39 @@ JeopardyGrid.prototype.fetchFromJService = function (endpoint) {
         })
 }
 
-JeopardyGrid.prototype.drawQuestions = function (values) {
-    for (let v = 0; v < 5; v++) {
-        //this.grid[rowNum][currentCol].element.textContent = rowNum * 200;
-        //console.log(values[v])
+JeopardyGrid.prototype.drawQuestions = function (category, catIndex) {
+    for (let questionIndex = 0; questionIndex < 5; questionIndex++) {
+        const currentSquare = this.board[catIndex][questionIndex + 1].element
+        currentSquare.textContent = ((questionIndex + 1) * 100)
+        //console.log(category[questionIndex].question)
+        this.eventListener(currentSquare, category)
     }
+}
+
+JeopardyGrid.prototype.eventListener = function (currentSquare, category) {
+    currentSquare.addEventListener("click", this.handle_click, category)
+}
+
+let currentQuestionAnswer = []
+let cellClickedCoordinates = []
+let beenClicked = false
+JeopardyGrid.prototype.handle_click = function (event) {
+    console.log(beenClicked)
+    const cellClicked = event.target
+    if (beenClicked === false && !cellClicked.classList.contains("completedQuestion")) {
+        currentQuestionAnswer.shift()
+        cellClickedCoordinates.shift()
+        cellClicked.classList.add("chosenQuestion")
+        const questionTextBox = document.getElementById("questionTextBox")
+        questionTextBox.textContent = onBoardCategories[cellClicked.dataset.rowIndex - 1][cellClicked.dataset.colIndex].question +
+            " ||| " + onBoardCategories[cellClicked.dataset.rowIndex - 1][cellClicked.dataset.colIndex].answer
+        currentQuestionAnswer.push(onBoardCategories[cellClicked.dataset.rowIndex - 1][cellClicked.dataset.colIndex].answer)
+        cellClickedCoordinates.push(cellClicked.dataset.rowIndex)
+        console.log(cellClickedCoordinates.toString())
+        beenClicked = true
+    } else {
+        return
+    }
+
+    console.log(beenClicked)
 }
